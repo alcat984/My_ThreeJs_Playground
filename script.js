aad4cf56dc22f10987c2ddaf433383dcc20ff535
Silvio Alejandro Catalano <silviocat84@gmail.com>
2022-08-27 19:46:22 +0200

    first exercise of ThreeJS

---
+++ script.js
@@ -0,0 +1,29 @@
+// Scene
+
+const scene = new THREE.Scene()
+
+//Red cube
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000'})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//sizes
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+//camera
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+console.log(canvas);
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
